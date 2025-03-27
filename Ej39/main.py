bcb67a84ba3017910cefff320c40c9f6669ff6a5
PabloHuerres01 <pablomartinezhuerres2001@gmail.com>
2025-03-27 20:10:54 +0100

    Ej39

--- Ej39/main.py
+++ Ej39/main.py
@@ -1,7 +1,13 @@
 class Main:
-    def binarioDecimal(self, texto):
-        numBinario=input("Introduce el numero binario\n")
+    def binarioDecimal(self):
+        numBinario = input("Introduce el numero binario\n")
+        indice = 0 
+        sol = 0
+        for num in reversed(numBinario):
+            sol += (2 ** indice) * int(num)
+            indice += 1
+        print(f"\nEl número decimal es {sol}")  
 
 if __name__ == "__main__":
     app = Main()
-    app.binarioDecimal
+    app.binarioDecimal()
